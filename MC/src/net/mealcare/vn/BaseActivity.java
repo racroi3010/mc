59aa192abcff6f96022cd80029d08dce7d751381
my pet <my pet@mypet-PC>
2016-04-19 17:46:45 +0900

    first commit

---
+++ MC/src/net/mealcare/vn/BaseActivity.java
@@ -0,0 +1,28 @@
+package net.mealcare.vn;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public abstract class BaseActivity extends Activity {
+	protected abstract int onGetContentViewResource();
+	protected abstract void onInit();
+	
+	protected final int AUTO_HIDE_DELAY_MILLIS = 3000;
+	
+	protected Context mContext = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(onGetContentViewResource());
+		mContext = this;
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		
+		onInit();
+	}
+
+	
+}
